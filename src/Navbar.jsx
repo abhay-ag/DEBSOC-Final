6cf4b621e7359995cd0b17903974d206a9b75a58
abhay-ag <abhay0034.be21@chitkara.edu.in>
2022-05-12 13:51:25 +0530

    dynamic navbar made

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import './style.css'
 
-function Navbar() {
+function Navbar({title, links}) {
   return (
-    <div className="w-screen h-16 md:h-20 bg-white-rgba backdrop-blur-sm z-[9999] fixed top-0"></div>
+    <div className="w-screen h-16 md:h-20 bg-white-rgba backdrop-blur-sm z-[9999] fixed top-0 justify-center flex items-center">
+      <div className="flex flex-1 md:flex-[0.2_0_0%] font-bold text-white text-3xl box-border">DEBSOC</div>
+    </div>
   )
 }
 
