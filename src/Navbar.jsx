bfc73864e5822b91e078eaf9711f6e6e1d86bd73
abhay-ag <abhay0034.be21@chitkara.edu.in>
2022-05-14 13:57:55 +0530

    specific buttons targeted on the basis of link

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -11,13 +11,20 @@ function Navbar({title, links}) {
     console.log(currLoc);
 
     document.title = currLoc === '' ? 'Home' : currLoc
+
+    const but = document.querySelectorAll(`a[href = '${str}']`)
+    const acLink = document.querySelectorAll('a')
+    acLink.forEach(active => {
+      active.classList.remove('bg-black-rgba')
+    })
+    but.forEach(buts => {
+      buts.classList.add('bg-black-rgba')
+    })
   })
   return (
     <div className="w-screen h-16 md:h-20 bg-white-rgba backdrop-blur-md z-[9999] fixed top-0 justify-center flex items-center">
       <div className="flex px-5 md:px-0 flex-1 md:flex-[0.1_0_0%] font-bold text-white text-2xl box-border">
-        <Link to='/'>
           {title}
-        </Link>
       </div>
       <div className="flex flex-1 md:flex-[0.6_0_0%] font-medium text-white text-xl box-border">
         {links?.map(link => <Link to={`${link === 'Home' ? '/' : link }`} className={`hidden md:block box-border px-4 py-2 mx-2 rounded-xl hover:bg-black-rgba transition duration-150 ease-out`}>{link}</Link>)}
