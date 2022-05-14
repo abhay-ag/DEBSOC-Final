2d0e466ae76aa3b397eccbd474a7088eec613582
abhay-ag <abhay0034.be21@chitkara.edu.in>
2022-05-14 11:18:05 +0530

    top position changed

--- src/Main.jsx
+++ src/Main.jsx
@@ -5,7 +5,7 @@ import HeroLogo from './imgs/hero.svg'
 function Main() {
   return (
     <div className="w-screen h-screen bg-bgb relative">
-      <img src={HeroLogo} alt = "Hero Gradient Banner" className='absolute w-full top-[-50px] z-[0]'/>
+      <img src={HeroLogo} alt = "Hero Gradient Banner" className='absolute w-full top-[-65px] z-[0]'/>
     </div>
   )
 }
