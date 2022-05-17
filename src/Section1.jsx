119899bb3459393bc399f26f0f8599a4ce6b0474
abhay-ag <abhay0034.be21@chitkara.edu.in>
2022-05-17 15:43:33 +0530

    basic heading made | test

--- src/Section1.jsx
+++ src/Section1.jsx
@@ -4,7 +4,7 @@ import './style.css'
 function Section1() {
   return (
     <div className="w-screen h-screen flex flex-col justify-center items-center rad-grad">
-      
+      <h1 className='text-7xl text-white font-bold'> Join Us</h1>
     </div>
   )
 }
