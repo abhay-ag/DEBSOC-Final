42dd45e40e92cdf2b697a72471a1d5fa8a4ab21c
abhay-ag <abhay0034.be21@chitkara.edu.in>
2022-05-18 10:25:49 +0530

    2 sections made | made dynamic | rendering done

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 
-function Card() {
+function Card({icon, text}) {
   return (
-    <div className='w-[30.5%] h-full flex flex-row items-center justify-center bg-gradient-to-br from-indigo-400 via-gray-700 to-gray-600 p-0.5 rounded-xl mt-5 box-border'>
-        <div className='w-full box-border rounded-xl p-5 flex font-bold text-white items-center justify-center flex-col h-full mycol'>
-            
+    <div className='w-full md:w-[30%] h-full flex flex-row items-center justify-center bg-gradient-to-br from-indigo-400 via-gray-700 to-gray-600 p-0.5 rounded mt-5 md:mt-0 box-border'>
+        <div className='w-full box-border rounded p-5 flex font-bold text-white items-center justify-center flex-col h-full mycol'>
+            <div className='text-magenta md:cuFon md:mt-4'>
+                {icon}
+            </div>
+            <div className='text-center font-normal mt-5 md:mb-5'>
+                {text}
+            </div>
         </div>
     </div>
   )
--- src/Section1.jsx
+++ src/Section1.jsx
@@ -1,15 +1,31 @@
 import React from 'react'
 import Card from './Card'
 import './style.css'
+import ChatIcon from '@mui/icons-material/Chat';
+import MenuBookIcon from '@mui/icons-material/MenuBook';
+import PeopleAltIcon from '@mui/icons-material/PeopleAlt';
+import { Link } from 'react-router-dom';
 
 function Section1() {
   return (
-    <div className="w-screen h-screen flex flex-col justify-center items-center rad-grad">
-      <h1 className='text-7xl text-white font-bold underline underline-offset-8 decoration-magenta'>Join DEBSOC</h1>
-      <h3 className='text-2xl w-1/2 text-white font-medium mt-8 text-center'>Kickstart your debating career with peer and project driven approach and get debating opportunities in top events and universities.</h3>
-      <div className='flex flex-row w-3/4'>
-        <Card />
+    <div className="w-screen h-fit md:h-[80vh] flex flex-col justify-center items-center rad-grad">
+      <h1 className='text-[10vw] md:text-7xl text-white font-bold underline underline-offset-8 decoration-magenta'>Join DEBSOC</h1>
+      <h3 className='text-normal md:text-2xl w-[90%] md:w-1/2 text-white font-medium mt-8 text-center'>Kickstart your debating career with peer and project driven approach and get debating opportunities in top events and universities.</h3>
+      <div className='flex flex-col md:flex-row justify-around w-[75%] mt-8 mb-5 md:mb-0 h-fit'>
+        <Card 
+          icon={<ChatIcon fontSize = 'large'/>}
+          text  = "Daily Live Classes and Doubt Sessions with hands on projects"
+        />
+        <Card 
+          icon={<MenuBookIcon fontSize='large' />}
+          text = "Most Active learning environment, Learn with friends and be part of the most supportive community"
+        />
+        <Card 
+          icon={<PeopleAltIcon fontSize='large' />}
+          text = "Mentorship from Industry experts of top debating societies"
+        />
       </div>
+      <Link className='link' to= '/apply'><button className='bg-magenta px-4 py-2 rounded text-xl mt-8 mb-5 font-semibold text-white'>Apply Now</button></Link>
     </div>
   )
 }
