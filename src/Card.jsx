fb32084c55942f28bdaab3adcffde0be7e6d0c10
abhay-ag <abhay0034.be21@chitkara.edu.in>
2022-05-18 09:34:21 +0530

    Card Made | making dynamic

---
+++ src/Card.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Card() {
+  return (
+    <div className='w-[30.5%] h-full flex flex-row items-center justify-center bg-gradient-to-br from-indigo-400 via-gray-700 to-gray-600 p-0.5 rounded-xl mt-5 box-border'>
+        <div className='w-full box-border rounded-xl p-5 flex font-bold text-white items-center justify-center flex-col h-full mycol'>
+            Test
+        </div>
+    </div>
+  )
+}
+
+export default Card
--- src/Section1.jsx
+++ src/Section1.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
+import Card from './Card'
 import './style.css'
 
 function Section1() {
   return (
     <div className="w-screen h-screen flex flex-col justify-center items-center rad-grad">
-      <h1 className='text-7xl text-white font-bold'> Join Us</h1>
+      <h1 className='text-7xl text-white font-bold underline underline-offset-8 decoration-magenta'>Join DEBSOC</h1>
+      <h3 className='text-2xl w-1/2 text-white font-medium mt-8 text-center'>Kickstart your debating career with peer and project driven approach and get debating opportunities in top events and universities.</h3>
+      <div className='flex flex-row w-3/4'>
+        <Card />
+      </div>
     </div>
   )
 }
