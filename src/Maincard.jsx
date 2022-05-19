261f0b7f427ded0c26507e41441fd8e4d398f018
abhay-ag <abhay0034.be21@chitkara.edu.in>
2022-05-19 09:40:51 +0530

    card layout made | making dynamic | section 5 started

--- src/Maincard.jsx
+++ src/Maincard.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import Border from './imgs/box-corner.svg'
+import CompSVG from './imgs/Computer'
 
-function Maincard() {
+function Maincard({icon, heading, desc}) {
   return (
-    <div className='w-[30rem] h-[15rem] rounded flex flex-col justify-center items-center p-0.5 bg-gradient-to-br from-indigo-400 via-gray-700 to-gray-600 p-0.5'>
-        <div className='w-full h-full mycol relative'>
-            <img src={Border} alt="corner border" className='absolute right-[-0.3rem] opacity-40 top-[-0.3rem]'/>
+    <div className='w-[27rem] h-[18rem] rounded flex flex-col justify-center items-center bg-gradient-to-br from-indigo-400 via-gray-700 to-gray-600 p-0.5'>
+        <div className='w-full h-full mycol relative flex flex-col justify-center box-border p-5'>
+            <img src={Border} alt="corner border" className='absolute right-[-0.3rem] opacity-30 top-[-0.3rem] z-0'/>
+            <div className='rounded-xl bg-[rgba(255,255,255,0.08)] w-16 h-16 flex items-center justify-center'>
+                {icon}
+            </div>
+            <h1 className='text-2xl font-medium text-white mt-4'>{heading}</h1>
+            <h2 className='text-normal text-[rgb(150,150,153)] text-justify mt-4'>{desc}</h2>
         </div>
     </div>
   )
--- src/imgs/Computer.jsx
+++ src/imgs/Computer.jsx
@@ -2,8 +2,8 @@ import * as React from "react";
 
 const SVGComponent = (props) => (
   <svg
-    width={32}
-    height={32}
+    width={38}
+    height={38}
     viewBox="0 0 32 32"
     xmlSpace="preserve"
     xmlns="http://www.w3.org/2000/svg"
@@ -12,7 +12,7 @@ const SVGComponent = (props) => (
     <path
       d="M29 27H3a2 2 0 0 1-2-2V7a2 2 0 0 1 2-2h26a2 2 0 0 1 2 2v18a2 2 0 0 1-2 2zm-15 0h4v4h-4zm17-4H1m8 8h14"
       fill="none"
-      stroke="#000"
+      stroke="#fff"
       strokeLinejoin="round"
       strokeMiterlimit={10}
       strokeWidth={2}
