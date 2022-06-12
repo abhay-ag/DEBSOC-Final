de26cdf6c8963254515c1a42fab3d2c6050a49f0
CinematicGenius007 <ayush3642.be20@chitkara.edu.in>
2022-06-12 23:27:10 +0530

    back to indigo

--- src/resources/main.js
+++ src/resources/main.js
@@ -31,22 +31,22 @@ const Curriculum = [
   {
     title: 'Public Speaking',
     info: 'Begin your journey with simple 3v3 mock debates, beginner friendly topics and flexible speech timings.',
-    svg: <i className="fad fa-podium-star text-violet-500 text-4xl"></i>
+    svg: <i className="fad fa-podium-star text-indigo-400 text-4xl"></i>
   },
   {
     title: 'Conventional Debates',
     info: 'Participate in byte sized teams with new DEBSOC members every time, explore your compatibility & work as teams.',
-    svg: <i className="fad fa-users text-violet-500 text-4xl"></i>
+    svg: <i className="fad fa-users text-indigo-400 text-4xl"></i>
   },
   {
     title: 'Individual Debates',
     info: 'Learn the art of POIs with group discussion events. Be ready for questions thrown at you!',
-    svg: <i className="fad fa-user text-violet-500 text-4xl"></i>
+    svg: <i className="fad fa-user text-indigo-400 text-4xl"></i>
   },
   {
     title: 'Parliamentary Debating',
     info: 'Get ready for Universally accepted and followed Asian & British Parliamentary Debating formats. By now you’re not short of words & a master of whipping the opposition with counter arguments.',
-    svg: <i className="fad fa-users-crown text-violet-500 text-4xl"></i>
+    svg: <i className="fad fa-users-crown text-indigo-400 text-4xl"></i>
   }
 ];
 
@@ -54,32 +54,32 @@ const Uniqueness = [
   {
     title: "Structured Curriculum",
     info: "Your progress is our mission and that’s fueled with a well laid out roadmap extremely suitable for beginners and expert debaters alike. ",
-    svg: <i className="fal fa-sitemap text-violet-500 text-3xl"></i>
+    svg: <i className="fal fa-sitemap text-indigo-400 text-3xl"></i>
   },
   {
     title: "Competitions",
     info: "If you are someone who’s even remotely competitive, we assure you’re up for a treat! Inter-club, inter-university, MUNs, novice, national and international events, we have it all :3",
-    svg: <i className="fad fa-swords text-violet-500 text-3xl"></i>
+    svg: <i className="fad fa-swords text-indigo-400 text-3xl"></i>
   },
   {
     title: "Social",
     info: "We know how much you’d love to meet new people as much as we do, so we pair you up with fresh faces in every event!",
-    svg: <i className="fad fa-handshake text-violet-500 text-3xl"></i>
+    svg: <i className="fad fa-handshake text-indigo-400 text-3xl"></i>
   },
   {
     title: "Themes",
     info: "If you’re an enthusiast for general knowledge & can’t start your day without knowing what’s happening around, then you’ll relish our smokin’ hot basket of topics curated straight out of matters that are relevant, fresh and thought provoking.",
-    svg: <i className="fal fa-books text-violet-500 text-3xl"></i>
+    svg: <i className="fal fa-books text-indigo-400 text-3xl"></i>
   },
   {
     title: "Exclusive",
     info: "DEBSOC is well known for its intense, comprehensive and lengthy interview process. Cutting no corners to establish an elite group of brilliant minds.",
-    svg: <i className="fal fa-crown text-violet-500 text-3xl"></i>
+    svg: <i className="fal fa-crown text-indigo-400 text-3xl"></i>
   },
   {
     title: "Life Long Skills",
     info: "Debating not just makes you a great speaker, but enriches your mind with the spirit of question, a belief in oneself and everlasting confidence. Being at The Debating Society is more than just a tag of excellence, shaping you for life.",
-    svg: <i className="fad fa-tools text-violet-500 text-3xl"></i>
+    svg: <i className="fad fa-tools text-indigo-400 text-3xl"></i>
   }
 ];
 
